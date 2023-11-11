64ae25136b3efb61bd4d8c79679547ef4a573e44
caisilus <serangor@gmail.com>
2023-11-11 16:14:19 +0300

    Add RepoController that clones forked repo and merges with upstream

--- lib/obsidian_article_publisher.rb
+++ lib/obsidian_article_publisher.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 require_relative "obsidian_article_publisher/link_converter"
+require "octokit"
+require_relative "obsidian_article_publisher/repo_controller"
 
 module ObsidianArticlePublisher
+  WORKDIR = "workdir"
+
   def publish
-    puts "publish..."
+    api = Octokit::Client.new(netrc: true)
+    Dir.mkdir(WORKDIR) unless Dir.exist?(WORKDIR)
+    @repo_controller = RepoController.new(owner: "caisilus", repo_name: "mmcs_ruby_guides", api:)
   end
 
   module_function :publish
 end
+
+ObsidianArticlePublisher.publish
